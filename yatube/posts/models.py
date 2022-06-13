fd64f5996d309c6e6e07349afe84d74c5891e4af
Евгений Цветов <jkehtttt@yandex.ru>
2022-06-14 00:40:37 +0300

    Финальный проект 6 спринт исправление замечаний2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -97,6 +97,11 @@ class Follow(models.Model):
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=("user", "author"), name="unique_user_in_author"
+            )
+        ]
 
     def __str__(self):
         return f'{self.user}{self.author}'
